1eed9e6edb34d4402beb874b15e07a4d962c3cd7
Anja Scherwall <anjagutt@hotmail.com>
2026-02-18 13:56:30 +0100

    ADD: Calculations, create csv-files and comments
    Calculate information for analysis and create new columns for the result. Create different csv-files to show result and possibillity to compare. Comments for section information.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from xmlrpc.client import Boolean
+
 import pandas as pd
 
 if __name__ == '__main__':
@@ -5,7 +7,9 @@ if __name__ == '__main__':
     # Read the csv file and set delimiter -> ;
     products_df = pd.read_csv("lab 1 - csv.csv", sep = ";")
 
-
+    """ 
+    Clean the data
+    """
     # Clean data in 'name'
     products_df["name"] = (products_df["name"]
                            .str.strip()
@@ -23,7 +27,10 @@ if __name__ == '__main__':
                                                errors="coerce",
                                                format = "%Y-%m-%d")
 
-    #print(products_df)
+
+    """ 
+    Create conditions for flagging and rejecting data
+    """
 
 
     # Create: Conditions for flagging data
@@ -42,24 +49,46 @@ if __name__ == '__main__':
             (products_df["created_at"].isna())
     )
 
-    # Reason for flagg
-    products_df["reason"] = ""
 
-    products_df.loc[products_df["price"] > 10000, "reason"] = "HIGH PRICE"
-    products_df.loc[products_df["price"] == 0, "reason"] = "NO COST"
+    """ 
+    Create status messages
+    """
+
+    # Status message
+    products_df["status"] = ""
+
+    products_df.loc[products_df["id"] != "NaN", "status"] = "VALID"
+
+    # Flagg reasons
+    products_df.loc[products_df["price"] > 10000, "status"] = "HIGH PRICE"
+    products_df.loc[products_df["price"] == 0, "status"] = "NO COST"
+
+    # Reject reasons
+    products_df.loc[products_df["id"].isna(), "status"] = "MISSING ID"
+    products_df.loc[products_df["name"].isna(), "status"] = "MISSING NAME"
+    products_df.loc[products_df["price"].isna(), "status"] = "MISSING PRICE"
+    products_df.loc[products_df["price"] < 0, "status"] = "NEGATIVE PRICE"
+    products_df.loc[products_df["currency"].isna(), "status"] = "MISSING CURRENCY"
+    products_df.loc[products_df["created_at"].isna(), "status"] = "MISSING CREATED_AT"
 
 
-    # Separate the data
+    """ 
+    Separating the data
+    """
+
     df_rejected = products_df[reject_conditions].copy()
     df_flagged = products_df[flagged_conditions].copy()
     df_valid = products_df[~reject_conditions & ~flagged_conditions].copy()
 
     # For calculations with flagged data, show the difference with high/no price products
-    df_valid_with_flaggs = products_df[~reject_conditions].copy()
+    df_valid_fl = products_df[~reject_conditions].copy()
 
 
-
-    #print(df_valid)
+    """ 
+    Create file for analytic summary:
+        - analytics with only valid data
+        - analytics with valid data AND flagged data for comparison 
+    """
 
     #Create:
     df_summary = pd.DataFrame({
@@ -73,31 +102,73 @@ if __name__ == '__main__':
 
 
     # Show the difference in analytics with high/no price products
-    df_summary_with_flaggs = pd.DataFrame({
-        "Mean Price": [df_valid_with_flaggs["price"].mean()],
-        "Median Price": [df_valid_with_flaggs["price"].median()],
-        "Product Amount": [len(df_valid_with_flaggs)],
+    df_summary_fl = pd.DataFrame({
+        "Mean Price": [df_valid_fl["price"].mean()],
+        "Median Price": [df_valid_fl["price"].median()],
+        "Product Amount": [len(df_valid_fl)],
         "Without Price Not Counted": [len(df_flagged)]
     })
 
-    df_summary_with_flaggs.to_csv("data/analytic_summary_with_flaggs.csv", index=False)
+    df_summary_fl.to_csv("data/analytic_summary_fl.csv", index=False)
+
+
+    """ 
+    Create file for price analysis:
+        - analysis with only valid data
+        - analysis with valid data AND flagged data for comparison 
+    """
+
+    # Create column for most expensive products
+    df_valid["Most Expensive"] = ""
+    df_valid.loc[df_valid["currency"] == "SEK", "Most Expensive"] = "N"
+
+    # Calculate the most expensive products
+    # For loop and enumerate to give each a nr, highest == 1 ...
+    for i, n in enumerate(df_valid.nlargest(10, "price").index, start=1):
+        df_valid.loc[n, "Most Expensive"] = f"ME {i}"
+
+    # Create column for most deviated products comparing to median
+    df_valid["Most Deviating"] = ""
+    median = df_valid["price"].median()
+
+    df_valid["difference from median"] = (df_valid["price"] - median).abs()
+    df_valid.loc[df_valid["currency"] == "SEK", "Most Deviating"] = "N"
+
+    # For loop and enumerate to give a nr, furthest from median == 1 ...
+    for i, n in enumerate(
+            df_valid.sort_values(by=["difference from median", "price"], ascending=[False, True]).head(10).index,
+            start=1):
+        df_valid.loc[n, "Most Deviating"] = f"MD {i}"
+
+    # Put the result in csv-file
+    df_valid.to_csv("data/price_analysis.csv", index=False)
 
 
 
+    # Same code including flagged products
+    df_valid_fl["Most Expensive"] = ""
+    df_valid_fl.loc[df_valid_fl["currency"] == "SEK", "Most Expensive"] = "N"
+    for i, n in enumerate(df_valid_fl.nlargest(10, "price").index, start=1):
+        df_valid_fl.loc[n, "Most Expensive"] = f"ME {i}"
+
+    # Create column for most deviated products comparing to median
+    df_valid_fl["Most Deviating"] = ""
+    median = df_valid_fl["price"].median()
+
+    df_valid_fl["difference from median"] = (df_valid_fl["price"] - median).abs()
+    df_valid_fl.loc[df_valid_fl["currency"] == "SEK", "Most Deviating"] = "N"
+
+    for i, n in enumerate(
+            df_valid_fl.sort_values(by=["difference from median", "price"], ascending=[False, True]).head(10).index,
+            start=1):
+        df_valid_fl.loc[n, "Most Deviating"] = f"MD {i}"
+
+    df_valid_fl.to_csv("data/price_analysis_fl.csv", index=False)
 
 
-    # Reason for rejection
-    df_rejected["reason"] = ""
-
-    df_rejected.loc[products_df["id"].isna(), "reason"] = "MISSING ID"
-    df_rejected.loc[products_df["name"].isna(), "reason"] = "MISSING NAME"
-    df_rejected.loc[products_df["price"].isna(), "reason"] = "MISSING PRICE"
-    df_rejected.loc[products_df["price"] < 0, "reason"] = "NEGATIVE PRICE"
-    df_rejected.loc[products_df["currency"].isna(), "reason"] = "MISSING CURRENCY"
-    df_rejected.loc[products_df["created_at"].isna(), "reason"] = "MISSING CREATED_AT"
-
-
-    #print(df_rejected)
+    """ 
+    Create file for rejected data 
+    """
 
     # Create: csv-file for rejected data
     df_rejected.to_csv("data/rejected_products.csv", index=False)
